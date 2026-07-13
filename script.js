433086da1f6c5a2a5b58841760feb6664dd74c8a
raihoes <260322g@mymail.nyp.edu.sg>
2026-07-13 13:24:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let input = "";
 
+function updateString(value) {
+
+    input = input + value;
+
+    document.getElementById("inputString").value = input;
+
+}
+
+function calculate() {
+
+    let result = eval(input);
+
+    document.getElementById("inputString").value = result;
+
+    input = "";
+
+}
+
+function resetString() {
+
+    input = "";
+
+    document.getElementById("inputString").value = "";
+
+}
