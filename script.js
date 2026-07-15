45bc1b4cc7463ddf306f2e34b1bcff0c9da02bfa
raihoes <260322g@mymail.nyp.edu.sg>
2026-07-15 15:32:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-let input = "";
+function calculateBudget() {
 
-function updateString(value) {
+    let flight = parseInt(document.getElementById("flight").value);
+    let hotel = parseInt(document.getElementById("hotel").value);
+    let food = parseInt(document.getElementById("food").value);
+    let transport = parseInt(document.getElementById("transport").value);
+    let activities = parseInt(document.getElementById("activities").value);
 
-    input = input + value;
+    if (document.getElementById("flight").value == "") {
+        flight = 0;
+    }
 
-    document.getElementById("inputString").value = input;
+    if (document.getElementById("hotel").value == "") {
+        hotel = 0;
+    }
+
+    if (document.getElementById("food").value == "") {
+        food = 0;
+    }
+
+    if (document.getElementById("transport").value == "") {
+        transport = 0;
+    }
+
+    if (document.getElementById("activities").value == "") {
+        activities = 0;
+    }
+
+    let sum = flight + hotel + food + transport + activities;
+
+    document.getElementById("totalCost").innerHTML = "$" + sum;
 
 }
 
-function calculate() {
-
-    let result = eval(input);
-
-    document.getElementById("inputString").value = result;
-
-    input = "";
-
-}
-
-function resetString() {
-
-    input = "";
-
-    document.getElementById("inputString").value = "";
-
-}
